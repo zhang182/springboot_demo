4c030890e2b3285e0fd1bc34df7a6d25f3cdbf98
pradox <1027112205@qq.com>
2025-04-18 11:03:37 +0800

    bug 版本的jav文件的修改，并且跟main分支的java文件同名

---
+++ text.java
@@ -0,0 +1,13 @@
+public class text {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World bug" );
+    }
+    public text(){
+
+        System.out.println("text");
+
+    }
+    //这是bug版本的java文件1
+}
