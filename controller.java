e1b5a8894458f8aa4c2f9ef1f265745de0ba12a7
pradox <1027112205@qq.com>
2025-04-18 14:56:17 +0800

    controller 000change更改

--- controller.java
+++ controller.java
@@ -3,6 +3,7 @@ public class controller {
 
     public static void main(String[] args) {
 
-        System.out.println("hello change000");
+        System.out.println("hello change");
+        System.out.println("hello change001");
     }
 }
